60f4534503c088ad7c36b5ddf117ed63af8c8391
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-09-01 00:58:31 +0200

    make build

---
+++ index.5f6dc69f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth/2-t.offsetWidth/2,i=e.clientHeight/2-t.offsetHeight/2;t.style.left=l+"px",t.style.top=i+"px";
+//# sourceMappingURL=index.5f6dc69f.js.map
